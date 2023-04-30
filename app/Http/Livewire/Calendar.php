e44123d5c79542a7552544f58b4b04b2aee8c53a
teraiken <k_terai@aivick.co.jp>
2023-04-30 23:15:41 +0900

    イベント計算

--- app/Http/Livewire/Calendar.php
+++ app/Http/Livewire/Calendar.php
@@ -9,39 +9,53 @@ use Livewire\Component;
 
 class Calendar extends Component
 {
-    public Carbon $currentDate;
+    public string $currentDate;
     public array $currentWeek = [];
     public string $day;
+    public string $checkDay;
+    public string $dayOfWeek;
     public Carbon $sevenDaysLater;
     public Collection $events;
 
     public function mount()
     {
-        $this->currentDate = Carbon::today();
+        $this->currentDate = Carbon::today()->format('Y-m-d');
         $this->sevenDaysLater = Carbon::today()->addDays(7);
 
         for ($i = 0; $i < 7; $i++) {
             $this->day = Carbon::today()->addDays($i)->format('m月d日');
-            array_push($this->currentWeek, $this->day);
+            $this->checkDay = Carbon::today()->addDays($i)->format('Y-m-d');
+            $this->dayOfWeek = Carbon::today()->addDays($i)->dayName;
+            array_push($this->currentWeek, [
+                'day' => $this->day,
+                'checkDay' => $this->checkDay,
+                'dayOfWeek' => $this->dayOfWeek,
+            ]);
         }
     }
 
     public function getDate(string $date)
     {
-        $this->currentDate = Carbon::parse($date);
+        $this->currentDate = $date;
         $this->sevenDaysLater = Carbon::parse($date)->addDays(7);
         $this->currentWeek = [];
 
         for ($i = 0; $i < 7; $i++) {
             $this->day = Carbon::parse($date)->addDays($i)->format('m月d日');
-            array_push($this->currentWeek, $this->day);
+            $this->checkDay = Carbon::parse($date)->addDays($i)->format('Y-m-d');
+            $this->dayOfWeek = Carbon::parse($date)->addDays($i)->dayName;
+            array_push($this->currentWeek, [
+                'day' => $this->day,
+                'checkDay' => $this->checkDay,
+                'dayOfWeek' => $this->dayOfWeek,
+            ]);
         }
     }
 
     public function render()
     {
         $this->events = Event::query()
-            ->whereBetween('start_date', [$this->currentDate->format('Y-m-d'), $this->sevenDaysLater->format('Y-m-d')])
+            ->whereBetween('start_date', [$this->currentDate, $this->sevenDaysLater->format('Y-m-d')])
             ->orderBy('start_date')
             ->get();
 
